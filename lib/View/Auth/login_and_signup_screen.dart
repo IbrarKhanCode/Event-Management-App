7b58b5ed6cdff0b564ef6c347ccf6dc381ac47dc
Ibrar Khan <ibrarkhan431414@gmail.com>
2025-03-17 22:15:49 +0500

    updated

---
+++ lib/View/Auth/login_and_signup_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginAndSignupScreen extends StatefulWidget {
+  const LoginAndSignupScreen({super.key});
+
+  @override
+  State<LoginAndSignupScreen> createState() => _LoginAndSignupScreenState();
+}
+
+class _LoginAndSignupScreenState extends State<LoginAndSignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
