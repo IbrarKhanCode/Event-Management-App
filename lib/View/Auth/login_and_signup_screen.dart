3e48e2f3a6eb22fbd78c9eb1a2bb8d315426882b
Ibrar Khan <ibrarkhan431414@gmail.com>
2025-03-23 15:13:51 +0500

    updated

--- lib/View/Auth/login_and_signup_screen.dart
+++ lib/View/Auth/login_and_signup_screen.dart
@@ -7,12 +7,44 @@ class LoginAndSignupScreen extends StatefulWidget {
   State<LoginAndSignupScreen> createState() => _LoginAndSignupScreenState();
 }
 
-class _LoginAndSignupScreenState extends State<LoginAndSignupScreen> {
+class _LoginAndSignupScreenState extends State<LoginAndSignupScreen> with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+  String _headerText = 'Login';
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(_updateHeaderText);
+  }
+
+  void _updateHeaderText(){
+    setState(() {
+      _headerText = _tabController.index == 0 ? 'Login' : 'Sign In';
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_updateHeaderText);
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
 
+      body: Column(
+        children: [
+          SizedBox(height: 50,),
+          Text(_headerText,style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,fontSize: 20),),
+          SizedBox(height: 50,),
+
+        ],
+      ),
     );
   }
 }
