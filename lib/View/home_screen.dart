34e66d864768f3e48be53469c15ded913abf5b1f
Ibrar Khan <ibrarkhan431414@gmail.com>
2025-04-17 19:23:57 +0500

    new files added

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -1,17 +1,53 @@
+import 'package:event_management_app/Controller/data_controller.dart';
+import 'package:event_management_app/Services/notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:event_management_app/View/Widgets/events_feed_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  DataController dataController = Get.find<DataController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home Screen'),),
+      backgroundColor: Colors.black.withOpacity(0.03),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: double.infinity,
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                // CustomAppBar(),
+
+                Text(
+                  "What Going on today",
+                  style: GoogleFonts.raleway(
+                      fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                SizedBox(
+                  height: Get.height * 0.02,
+                ),
+                EventsFeed(),
+                Obx(()=> dataController.isUsersLoading.value? Center(child: CircularProgressIndicator(),) : EventsIJoined())
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+
+
 }
